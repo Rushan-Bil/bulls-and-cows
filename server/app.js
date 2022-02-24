714c8bd69fae4e3931a10df5e488cf77a284cf34
AlexGunin <iwaannafly@mail.ru>
2022-02-24 11:22:58 +0300

    start template

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const PORT = process.env.PORT ?? 3001;
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT);
