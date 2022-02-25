705d0b7298889cbef397433b6866a38ba2941413
Rushan <rushan.bil@gmail.com>
2022-02-25 19:17:28 +0300

    added train mode

--- client/src/components/pages/TrainPage/TrainPage.jsx
+++ client/src/components/pages/TrainPage/TrainPage.jsx
@@ -4,17 +4,21 @@ import WordsList from '../../WordsList/WordsList';
 import Alphabet from '../../Alphabet/Alphabet';
 import WordInput from '../../WordInput/WordInput';
 import LetterContainer from '../../LetterContainer/LetterContainer';
+import StartModal from '../../StartModal/StartModal';
 
 function TrainPage() {
   const [words, setWords] = useState([]);
+  const [secret, setSecret] = useState('');
+
   return (
     <>
       TrainPage
+      <StartModal setSecret={setSecret} />
       <div className={cls.trainPage}>
         <WordsList words={words} />
         <div className="flex-d-c column-3">
           <Alphabet />
-          <WordInput setWords={setWords} />
+          <WordInput secret={secret} setWords={setWords} />
         </div>
         <div className="flex-d-c column-3">
           <LetterContainer />
