bda6f824c1d9c654a9920301b251d60bef2c53f3
Rushan <rushan.bil@gmail.com>
2022-02-25 13:50:11 +0300

    added train page

--- client/src/components/WordInput/WordInput.jsx
+++ client/src/components/WordInput/WordInput.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import controller from '../../controllers/TrainController';
 
 function WordInput() {
+  const secret = 'qwert';
+  const wordLength = secret.length;
+
+  const [inputs, setInputs] = useState({});
+  const [words, setWords] = useState([]);
+
+  const inputsHandler = useCallback((e) => {
+    if (e.target.value.length <= wordLength) {
+      setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+  }, []);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let suppose = '';
+    suppose = inputs.suppose;
+    setInputs({});
+
+    if (suppose?.length === wordLength) {
+      console.log(secret);
+      console.log(suppose);
+      const bulls = controller.checkBulls(secret, suppose);
+      const cows = controller.checkCows(secret, suppose);
+      console.log(bulls, 'bulls');
+      console.log(cows, 'cows');
+      setWords((prev) => ([...prev, { word: suppose, bulls, cows }]));
+    } else {
+      alert('Введите слово целиком');
+    }
+  };
+  console.log(words);
+
   return (
-    <div className="flex-c-c row-2">
-      WordInput
+    <div className="inputForm">
+      <form onSubmit={submitHandler}>
+        <div className="">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="exampleFormControlInput1" className="form-label">Введите своё слово:</label>
+          <div>
+            <input
+              type="text"
+              className="form-control"
+              id="inputSuppose"
+              // pattern="[А-Яа-яЁё ]+" // ТАК СЕБЕ ВАРИАНТ
+              // placeholder="Только русские буквы" // ТАК СЕБЕ ВАРИАНТ
+              name="suppose"
+              onChange={inputsHandler}
+              value={inputs.suppose || ''}
+            />
+          </div>
+        </div>
+        <button type="submit">Проверить!</button>
+      </form>
     </div>
   );
 }
--- client/src/components/pages/TrainPage/TrainPage.jsx
+++ client/src/components/pages/TrainPage/TrainPage.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import cls from './train.module.css';
+import WordsList from '../../WordsList/WordsList';
+import Alphabet from '../../Alphabet/Alphabet';
+import WordInput from '../../WordInput/WordInput';
+import LetterContainer from '../../LetterContainer/LetterContainer';
 
 function TrainPage() {
   return (
-    <div>
+    <>
       TrainPage
-    </div>
+      <div className={cls.trainPage}>
+        <WordsList />
+        <div className="flex-d-c column-3">
+          <Alphabet />
+          <WordInput />
+        </div>
+        <div className="flex-d-c column-3">
+          <LetterContainer />
+          <LetterContainer />
+        </div>
+      </div>
+    </>
   );
 }
 
