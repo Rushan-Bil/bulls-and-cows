fe412afd2b79feea3d9a74e06261a7d972026928
AlexGunin <iwaannafly@mail.ru>
2022-02-26 12:18:10 +0300

    add animation and logic move letter

---
+++ client/src/components/Letter/Letter.jsx
@@ -0,0 +1,41 @@
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import cls from './letter.module.css';
+import { letterSlice } from '../../store/reducers/lettersSlice';
+import helper from '../../controllers/helper';
+
+function Letter({ value, typeContainer }) {
+  const [type, setType] = useState(typeContainer);
+  const dispatch = useDispatch();
+  const [timerId, setTimerId] = useState(null);
+
+  const { replace } = letterSlice.actions;
+
+  const changeType = () => {
+    switch (type) {
+      case 'wrong':
+        return setType('doubt');
+      case 'doubt':
+        return setType('correct');
+      case 'correct':
+        return setType('alphabet');
+      case 'alphabet':
+        return setType('wrong');
+      default:
+        return setType('wrong');
+    }
+  };
+
+  const clickHandler = () => {
+    dispatch(replace({ to: helper.setCorrectType(type), from: typeContainer, letter: value }));
+  };
+  const debouncedClickHandler = helper.debounce(changeType, clickHandler, 1000, timerId, setTimerId);
+
+  return (
+    <div className={['letter', cls.letter, cls[type]].join(' ')} onClick={debouncedClickHandler}>
+      {value}
+    </div>
+  );
+}
+
+export default Letter;
--- client/src/store/reducers/lettersSlice.js
+++ client/src/store/reducers/lettersSlice.js
@@ -1,15 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
+  alphabet: [],
   wrong: [],
   doubt: [],
   correct: [],
 };
 
 export const letterSlice = createSlice({
-  name: 'user',
+  name: 'letter',
   initialState,
-  reducers: {},
+  reducers: {
+    setAlphabet(state, action) {
+      state.alphabet = action.payload;
+    },
+    replace(state, action) {
+      const { from, to, letter } = action.payload;
+      if (from === to) return;
+      state[to].push(letter);
+      state[to].sort();
+      state[from] = state[from].filter((item) => item !== letter);
+    },
+  },
 });
 
 export default letterSlice.reducer;
