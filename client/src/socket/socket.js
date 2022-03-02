260f96328dd687fd7167442bd6c2b7fbb4d1ac89
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2022-03-02 14:23:12 +0300

    fix socket

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,5 @@
 // const HOST = window.location.origin.replace(/^http/, 'ws');
 // HOST = HOST.replace(/:\d{3,5}/gi, '');
-const socket = new WebSocket('ws://localhost:3001');
+const socket = new WebSocket('wss://bulls-and-cows-apii.herokuapp.com');
 console.log('socket');
 export default socket;
