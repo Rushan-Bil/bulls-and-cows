2f92e6289519d9449acebfd027e0527cf8483472
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2022-03-02 09:54:34 +0300

    Deleted client

--- client/src/socket/socket.js
+++
@@ -1,5 +0,0 @@
-// const HOST = window.location.origin.replace(/^http/, 'ws');
-// HOST = HOST.replace(/:\d{3,5}/gi, '');
-const socket = new WebSocket('ws://localhost:3001');
-console.log('socket');
-export default socket;
